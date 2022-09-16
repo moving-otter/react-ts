938a882820f992e54bab87c457e320197e0b6d1c
Baeblue <seungmin941215@gmail.com>
2022-09-16 21:27:36 +0900

    refactor(comp): Apply styled-components to highlighter

--- src/@common/highlighter/Highlighter.tsx
+++ src/@common/highlighter/Highlighter.tsx
@@ -1,5 +1,5 @@
+import styled from 'styled-components'
 import React, {Component} from 'react';
-import './Highlighter.scoped.scss';
 import 'highlight.js/scss/androidstudio.scss';
 import hljs from 'highlight.js/lib';
 
@@ -43,29 +43,90 @@ class Highlighter extends Component<props, state> {
 
   render() {
     return (
-        <pre className="highlighter">
-          <div className="titleArea hwUserSelectNone">
+        <Wrapper>
+          <TitleArea className="hwUserSelectNone">
             {this.props.codeInfo.desc}
-          </div>
+          </TitleArea>
 
-          <div className="content">
-            <div className="lineNumber hwUserSelectNone">
+          <Content>
+            <LineNumber className="hwUserSelectNone">
               {this.state.numList.map(num =>
-                <div className="num" key={num} tabIndex={1}>
+                <Num key={num} tabIndex={1}>
                   {num + 1}
-                </div>
+                </Num>
               )}
-            </div>
+            </LineNumber>
 
-            <code className={`${this.props.codeInfo.type} customCode`}>
-            {this.props.codeInfo.code}
-          </code>
-          </div>
+            <CustomCode className={`${this.props.codeInfo.type}`}>
+              {this.props.codeInfo.code}
+            </CustomCode>
+          </Content>
 
-          <div className="bottomLine"/>
-        </pre>
+          <BottomLine/>
+        </Wrapper>
     );
   }
 }
 
 export default Highlighter;
+
+const Wrapper = styled.pre`
+  position: relative;
+  padding: 20px 0;
+`;
+
+const TitleArea = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  top: 0;
+  left: 5px;
+  background-color: #3b3b3b;
+  padding: 5px 30px;
+  margin-left: -30px;
+  font-family: auto !important;
+
+  img {
+    width: 22px;
+    height: 22px;
+    filter: invert(0.7);
+    margin-right: 8px;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  line-height: 21px;
+`;
+
+const LineNumber = styled.div`
+  background-color: rgb(49, 51, 53);
+  color: rgb(96, 99, 102);
+  margin-left: -30px;
+  padding: 20px 0 40px 0;
+  margin-right: 30px;
+`;
+
+const Num = styled.div`
+  padding: 0 22px 0 10px;
+
+  &:hover {
+    background-color: #3e3e3e;
+  }
+
+  &:focus {
+    color: black;
+    background-color: silver;
+  }
+`;
+
+const CustomCode = styled.code`
+  background: none;
+  padding: 20px 0 40px 0;
+`;
+
+const BottomLine = styled.div`
+  border-bottom: 5px solid #313335;
+  margin: 0 -28px;
+  margin-top: -5px;
+`;
