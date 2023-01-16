8cdc692e42cbf4f1e218e8d3ec1c734f55f3ec70
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-17 01:33:29 +0900

    feat(firebase): Deploy content to firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,11 @@
 import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import firebase from "firebase/app";
 import rootReducer from "@store";
 import { Home } from "./home/Home";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-firebase.initializeApp({
-  apiKey: "AIzaSyC5NnEXy55OCkX0rY46TK-Nz2suCV201W4",
-  authDomain: "hw-site.firebaseapp.com",
-  projectId: "hw-site",
-  storageBucket: "hw-site.appspot.com",
-  messagingSenderId: "230730258467",
-  appId: "1:230730258467:web:a7ed10b992db3cc7477996"
-});
 
 console.log("[react-ts] build by ZOL");
 
