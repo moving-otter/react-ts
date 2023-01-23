221542798ac02df8413109b5886488436dd2fa94
Hyun-Woo Yoo <gusdn1460@nate.com>
2023-01-24 01:14:00 +0900

    Enable public path

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -218,7 +218,8 @@ module.exports = function (webpackEnv) {
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // publicPath: paths.publicUrlOrPath,
 
-      publicPath: './',
+      // publicPath: './',
+      publicPath: paths.publicUrlOrPath,
 
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
